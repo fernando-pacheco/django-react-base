707b017540900fa8493f02605a3142003d9dfda6
Fernando Pacheco <fernando.silva@sao.jamef.com.br>
2024-12-20 12:10:51 -0300

    [frontend] feat: retorno de response.data na service

--- frontend/src/service/base.ts
+++ frontend/src/service/base.ts
@@ -4,8 +4,8 @@ import { AxiosError } from "axios"
 export class BaseService {
     async get() {
         try {
-            const response = await api.get("/cliente")
-            return response
+            const response = await api.get("/base-int")
+            return response.data
         } catch (error) {
             if (error instanceof AxiosError) {
                 return error.response
